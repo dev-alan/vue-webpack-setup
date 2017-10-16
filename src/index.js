4989c4844e2a6f0959c846061c68a955ab18b837
Alan <alanl@sitesuite.com.au>
2017-10-16 11:43:27 +1100

    added loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 require('vue');
+require('bootstrap/scss/bootstrap.scss');
+require('./global.js');
+require('./styles.css');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,54 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.min.js',
     path: path.resolve(__dirname, 'dist/assets')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract([ 
+          { loader: 'css-loader' }
+        ])
+      },
+      {
+        test: /\.less$/i,
+        use: ExtractTextPlugin.extract([ 'css-loader', 'less-loader' ])
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          limit: 50000,
+          name: 'fonts/[name].[ext]'
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'img/[name].[ext]'
+            } 
+          }
+        ]
+      }
+    ]
+  },
+  plugins: 
+  [
+    new ExtractTextPlugin('styles.min.css'),
+  ]
 };
